eda840c35eaace9dbb97edfb3fc75cb894d6f097
MagdaZny <zieleznym6@gmail.com>
2019-04-28 00:43:19 +0200

    split getAllPosts and get OnePost to separate components

--- src/WeatherApiClient.js
+++ src/WeatherApiClient.js
@@ -1,21 +1,69 @@
 import React, { Component } from 'react';
 import './WeatherApiClient.css';
 
-export class FetchWeather extends Component {
+function Post(props) {
+    return (
+        <div className="Post">
+            <div className="PostTitle">{props.title}</div>
+            <div className="PostBody">{props.body}</div>
+        </div>
+    )
+}
+
+export class FetchAllPosts extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            number: props.number,
+        const post = {
             post: {
                 title: null,
                 body: null,
-            },
-            posts: [{
-                title: null,
-                body: null,
-            }]
+            }
+        };
+        this.state = {
+            posts: [post]
         }
     }
+    getAllPosts = (event) => {
+        event.preventDefault(); // May or may not be necessary depending on what you are doing
+
+        const endpoint = `https://jsonplaceholder.typicode.com/posts`;
+        fetch(endpoint)
+            .then((response) => response.json())
+            .then(data => {
+                this.setState({ posts: data })
+                console.log(this.state.posts)
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={(e) => this.getAllPosts(e)}>
+                    <button type='submit'>Get All Posts</button>
+                </form>
+                {this.state.posts.map((item, i) =>
+                    <Post title={item.title} body={item.body} />
+                )}
+            </div>
+        )
+    }
+}
+
+export class FetchOnePost extends Component {
+    constructor(props) {
+        super(props);
+        const post = {
+            post: {
+                title: null,
+                body: null,
+            }
+        };
+        this.state = {
+            post: post,
+            number: 0
+        }
+    }
+
     handleChange = (event) => {
         event.preventDefault(); // May or may not be necessary depending on what you are doing
 
@@ -29,48 +77,32 @@ export class FetchWeather extends Component {
             });
     }
 
-    getAllPosts = (event) => {
-        event.preventDefault(); // May or may not be necessary depending on what you are doing
-
-        const endpoint = `https://jsonplaceholder.typicode.com/posts`;
-
-        fetch(endpoint)
-            .then((response) => response.json())
-            .then(data => {
-                this.setState({ posts: data })
-                console.log(this.state.posts)
-            });
-    }
-
     render() {
         return (
-            <div className="WeatherMain">
+            <div>
                 <form onSubmit={(e) => this.handleSubmit(e)}>
                     <input
                         type='text'
-                        placeholder='someone@example.com'
-                        value={this.state.email}
+                        placeholder='Post id'
+                        value={this.state.value}
                         onChange={this.handleChange} />
                     <button type='submit'>Get One Post</button>
                 </form>
-                <form onSubmit={(e) => this.getAllPosts(e)}>
-                    <button type='submit'>Get All Posts</button>
-                </form>
-                <div className="Post">
-                    <div className="PostTitle">{this.state.post.title}</div>
-                    <div className="PostBody">{this.state.post.body}</div>
-                </div>
-                <div className="Posts">
-                    {this.state.posts.map((item, i) => 
-                    <div className="Post">
-                        <div className="PostTitle">{item.title}</div>
-                        <div className="PostBody">{item.body}</div>
-                    </div>
-                    )}
-                </div>
+                <Post title={this.state.post.title} body={this.state.post.body} />
+            </div>
+        )
+    }
+}
+
+export class FetchWeather extends Component {
+    render() {
+        return (
+            <div className="WeatherMain">
+                <FetchOnePost />
+                <FetchAllPosts />
             </div>
         );
     }
-
 }
+
 export default FetchWeather;
