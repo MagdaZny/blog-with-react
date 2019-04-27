8b02dbd2e5f855c30fe69978a2f203d1347238a4
MagdaZny <zieleznym6@gmail.com>
2019-04-28 00:15:59 +0200

    Return list of all posts

--- src/WeatherApiClient.js
+++ src/WeatherApiClient.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './WeatherApiClient.css';
 
 export class FetchWeather extends Component {
     constructor(props) {
@@ -6,9 +7,13 @@ export class FetchWeather extends Component {
         this.state = {
             number: props.number,
             post: {
-                id: null,
                 title: null,
-            }
+                body: null,
+            },
+            posts: [{
+                title: null,
+                body: null,
+            }]
         }
     }
     handleChange = (event) => {
@@ -19,34 +24,51 @@ export class FetchWeather extends Component {
 
         fetch(endpoint)
             .then((response) => response.json())
-            .then((data) => {
+            .then(data => {
                 this.setState({ post: data })
             });
     }
 
-    handleSubmit(e: any) {
-        e.preventDefault();
-        this.setState({
-          myArg: this.state.str
-        })
-      }
+    getAllPosts = (event) => {
+        event.preventDefault(); // May or may not be necessary depending on what you are doing
+
+        const endpoint = `https://jsonplaceholder.typicode.com/posts`;
+
+        fetch(endpoint)
+            .then((response) => response.json())
+            .then(data => {
+                this.setState({ posts: data })
+                console.log(this.state.posts)
+            });
+    }
 
     render() {
         return (
-            <div>
+            <div className="WeatherMain">
                 <form onSubmit={(e) => this.handleSubmit(e)}>
                     <input
                         type='text'
                         placeholder='someone@example.com'
                         value={this.state.email}
                         onChange={this.handleChange} />
-                    <button type='submit'>Check</button>
+                    <button type='submit'>Get One Post</button>
                 </form>
-                <div>
-                    <div>{this.state.post.title}</div>
-                    <div>{this.state.post.id}</div>
-                </div>)               
-                 </div>
+                <form onSubmit={(e) => this.getAllPosts(e)}>
+                    <button type='submit'>Get All Posts</button>
+                </form>
+                <div className="Post">
+                    <div className="PostTitle">{this.state.post.title}</div>
+                    <div className="PostBody">{this.state.post.body}</div>
+                </div>
+                <div className="Posts">
+                    {this.state.posts.map((item, i) => 
+                    <div className="Post">
+                        <div className="PostTitle">{item.title}</div>
+                        <div className="PostBody">{item.body}</div>
+                    </div>
+                    )}
+                </div>
+            </div>
         );
     }
 
